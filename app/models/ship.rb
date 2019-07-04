f28fe6809b0203ffabcb3315c860e55888a2f06e
Natalie <natsetc@gmail.com>
2019-07-03 17:03:30 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(details)
+        
+        details.each do |k, v|
+            self.send(("#{k}="), v)
+        end
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
